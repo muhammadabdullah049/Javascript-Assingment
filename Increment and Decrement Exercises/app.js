9a2ee3c581e33d230f28c45506f662c27728d8ff
muhammadabdullah049 <muhammadabdullah2612049@gmail.com>
2024-03-02 16:19:38 +0500

    Increment and Decrement Exercises Done

---
+++ Increment and Decrement Exercises/app.js
@@ -0,0 +1,39 @@
+
+
+//Q1
+var num = 8;
+console.log(num++);
+
+//Q2
+var x = 15;
+console.log(--x);
+
+//Q3
+var num = 25;
+console.log(num--);
+
+//Q4
+var num = 10;
+console.log(++num);
+
+//Q5
+
+
+
+//Q6
+var quantity = 12;
+console.log(--quantity);
+
+
+
+
+
+//Q8
+var score = 30;
+console.log(score--);
+
+//Q9
+var num = 20;
+console.log(++num);
+
+//Q10
