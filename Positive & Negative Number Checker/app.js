20ec06fe119e950ab681d9b303c96348b8b7673e
muhammadabdullah049 <muhammadabdullah2612049@gmail.com>
2024-03-05 16:00:33 +0500

    done

---
+++ Positive & Negative Number Checker/app.js
@@ -0,0 +1,12 @@
+
+//Q2
+const number = prompt("Enter a number: ")
+if (number > 10) {
+    console.log("The number is positive")
+}
+else if (number == 0) {
+    console.log("The number is zero");
+}
+else{
+    console.log("Tha number is negative")
+}
