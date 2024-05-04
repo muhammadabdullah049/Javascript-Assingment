0d033ca2f5a5e3f00dd08a6230eba7157cf3f3cf
muhammadabdullah049 <muhammadabdullah2612049@gmail.com>
2024-05-04 16:24:01 +0500

    countdown done

--- Assingments/app.js
+++ Assingments/app.js
@@ -3,20 +3,20 @@
 //Q1
 // *String Length Calculation*:
 // Write a program to calculate the length of a given string.
-var str = "adbjwnudcnelnon";
-console.log(str.length);
+// var str = "adbjwnudcnelnon";
+// console.log(str.length);
 
 //Q2
 // *String to Uppercase*:
 // Convert a string to uppercase.
-var upc = "abdullah";
-console.log(upc.toUpperCase());
+// var upc = "abdullah";
+// console.log(upc.toUpperCase());
 
 //Q3
 // *String to Lowercase*:
 //    Convert a string to lowercase.
-var lwc = "ABDULLAH";
-console.log(lwc.toLowerCase());
+// var lwc = "ABDULLAH";
+// console.log(lwc.toLowerCase());
 
 //Q4
 // *Count Specific Character*:
@@ -36,9 +36,9 @@ console.log(lwc.toLowerCase());
 //Q7
 // *Trim Whitespace*:
 // Remove leading and trailing whitespaces from a string.
-var email = "  abcd1235@gmail.com   ";
-console.log(email.length);
-console.log("Trim=>", email.trim().length);
+// var email = "  abcd1235@gmail.com   ";
+// console.log(email.length);
+// console.log("Trim=>", email.trim().length);
 
 // var sentence = "Javascript is Tha Wonderfull Programming Language.";
 // var count = 0
@@ -136,49 +136,49 @@ console.log("Trim=>", email.trim().length);
 
 
 //Color Changing Box on Click Or on Mouse Hover
-var box = document.getElementById ("box");
+// var box = document.getElementById ("box");
 
-console.log (box.style);
+// console.log (box.style);
 
-function changeRed() {
-    box.style.backgroundColor = "red";
-}
-function changeOrange() {
-    box.style.backgroundColor = "orange";
-}
-function changeBlue() {
-    box.style.backgroundColor = "blue";
-}
-function changeBlack() {
-    box.style.backgroundColor = "black";
-}
-function changeYellow() {
-    box.style.backgroundColor = "yellow";
-}
+// function changeRed() {
+//     box.style.backgroundColor = "red";
+// }
+// function changeOrange() {
+//     box.style.backgroundColor = "orange";
+// }
+// function changeBlue() {
+//     box.style.backgroundColor = "blue";
+// }
+// function changeBlack() {
+//     box.style.backgroundColor = "black";
+// }
+// function changeYellow() {
+//     box.style.backgroundColor = "yellow";
+// }
 
 
 
-// Word Reverser
+// // Word Reverser
 
-var rev = "abdullah";
-rev = [...rev].reverse().join("");
-console.log (rev);
+// var rev = "abdullah";
+// rev = [...rev].reverse().join("");
+// console.log (rev);
 
-// Word Capitalizer
+// // Word Capitalizer
 
-var userName = "abdullah abid hussain";
-var arr = userName.split(" ");
-console.log (userName);
-console.log (arr);
+// var userName = "abdullah abid hussain";
+// var arr = userName.split(" ");
+// console.log (userName);
+// console.log (arr);
 
-for (var i = 0; i < arr.length; i++){
-    var word = arr[i];
-    arr[i] = word[0].toUpperCase() + word.slice(1);
-}
+// for (var i = 0; i < arr.length; i++){
+//     var word = arr[i];
+//     arr[i] = word[0].toUpperCase() + word.slice(1);
+// }
 
-console.log (arr);
-var updated = arr.join(" ");
-console.log (updated);
+// console.log (arr);
+// var updated = arr.join(" ");
+// console.log (updated);
 
 // Word Finder
 
@@ -209,7 +209,136 @@ console.log (updated);
 
 // String Concatenation:
 
-var firstName = prompt("Enter Your First Name");
-var lastName = prompt("Enter Your Last Name");
-console.log (firstName.concat(lastName));
+// var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// console.log (firstName.concat(lastName));
 
+
+
+
+
+
+// var days = document.getElementById("days");
+// var hours = document.getElementById("hours");
+// var minutes = document.getElementById("minutes");
+// var seconds = document.getElementById("seconds");
+
+// var dd = document.getElementById("dd");
+// var hh = document.getElementById("hh");
+// var mm = document.getElementById("mm");
+// var ss = document.getElementById("ss");
+
+// var day_dot = document.querySelector("day_dot");
+// var hr_dot = document.querySelector("hr_dot");
+// var min_dot = document.querySelector("min_dot");
+// var sec_dot = document.querySelector("sec_dot");
+
+// var endDate = "06/17/24 00:00:00";
+// var x = setInterval(function(){
+//     var now = new Date(endDate).getTime();
+//     var countDown =new Date().getTime();
+//     var distance = now - countDown;
+    
+//     var d = Math.floor(distance / 1000 / 60 / 60 / 24);
+//     var h = Math.floor(distance / 1000 / 60 / 60);
+//     var m = Math.floor(distance / 1000 / 60);
+//     var s = Math.floor(distance / 1000);
+
+//     //Output the result in element with id
+//     days.innerText = d + "<br><span>Days</span>";
+//     hours.innerText = h + "<br><span>Hours</span>";
+//     minutes.innerText = m + "<br><span>Minutes</span>";
+//     seconds.innerText = s + "<br><span>Seconds</span>";
+
+//     //Animate Stroke
+//     dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+//     //365 Days in a year
+//     hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+//     //24 Hours in a day
+//     mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+//     //60 Minutes in a hour
+//     ss.style.strokeDashoffset = 440 - (440 * s) / 365;
+//     //60 Seconds in a minute
+
+
+//     //Animate circle dots
+//     day_dot.style.transform = `rotateZ(${d * 0.986}deg)`;
+//     //360deg / 365days = 0.986
+//     hr_dot.style.transform = `rotateZ(${h * 15}deg)`;
+//     //360deg / 24hours = 15
+//     min_dot.style.transform = `rotateZ(${m * 0.986}deg)`;
+//     //360deg / 60minutes = 6
+//     sec_dot.style.transform = `rotateZ(${s * 0.986}deg)`;
+//     //360deg / 60seconds = 6
+
+//     //if the countdown is over, write some text
+//     if(distance < 0){
+//         clearInterval(x);
+//         document.getElementById("time").style.display = 'none'
+//         document.querySelector(".eid").style.display = 'block';
+//     }
+// })
+
+
+
+
+var days = document.getElementById('days');
+var hours = document.getElementById('hours');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
+
+var dd = document.getElementById('dd');
+var hh = document.getElementById('hh');
+var mm = document.getElementById('mm');
+var ss = document.getElementById('ss');
+
+var day_dot = document.querySelector('day_dot');
+var hr_dot = document.querySelector('hr_dot');
+var min_dot = document.querySelector('min_dot');
+var sec_dot = document.querySelector('sec_dot');
+
+var endDate = '06/17/2024 00:00:00';
+//Date format mm/dd/yyyy
+var x = setInterval(function(){
+    var now = new Date(endDate).getTime();
+    var countDown = new Date().getTime();
+    var distance = now - countDown;
+
+    //Time calculation for days, hours, minutes, seconds
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var h = Math.floor((distance %  (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((distance %  (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((distance % (1000 * 60)) / (1000));
+
+    //Output the result in element with id
+    days.innerHTML = d + "<br><span>Days</span>";
+    hours.innerHTML = h + "<br><span>Hours</span>";
+    minutes.innerHTML = m + "<br><span>Minutes</span>";
+    seconds.innerHTML = s + "<br><span>Seconds</span>";
+
+    //Animate stroke
+    dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+    //365 Days in a year
+    hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+    //24 Hours in a day
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    //60 Minutes in a hour
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    //Animate circle dots
+    day_dot.style.transform = `rotateZ(${d * 0.986}deg)`;
+    //360 deg / 365days = 0.986
+    hr_dot.style.transform = `rotateZ(${h * 15}deg)`;
+    //360deg / 24hours = 15
+    min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+    //360deg / 60minutes
+    sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+    //360deg / 60seconds
+
+    //if countdown is over, write some text
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("time").style.display = 'none';
+        document.querySelector(".eidMubarak").style.display = 'block';
+    }
+})
